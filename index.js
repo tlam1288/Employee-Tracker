73872ba97ac9e42c8f83feaf50e545d24fda3020
nhuanthi <tlam1288@gmail.com>
2020-07-14 18:21:30 -0700

    added functions to view/add departments, view/add roles, view/add employees

--- index.js
+++ index.js
@@ -22,12 +22,35 @@ function startSearch() {
       name: "action",
       type: "list",
       message: "What would you like to do?",
-      choices: ["Add a new department", "Exit"],
+      choices: [
+        "Add a new department",
+        "View all departments",
+        "Add a new role",
+        "View all roles",
+        "Add an employee",
+        "View all employees",
+        "Exit",
+      ],
     })
     .then(function (answer) {
       switch (answer.action) {
-        case "What would you like to do?":
+        case "Add a new department":
           addNewDepartment();
+          break;
+        case "View all departments":
+          viewDepartments();
+          break;
+        case "Add a new role":
+          addNewRole();
+          break;
+        case "View all roles":
+          viewAllRoles();
+          break;
+        case "Add an employee":
+          addEmployee();
+          break;
+        case "View all employees":
+          viewAllEmployees();
           break;
         case "Exit":
           connection.end();
@@ -36,6 +59,7 @@ function startSearch() {
     });
 }
 
+//Add a new department
 function addNewDepartment() {
   inquirer
     .prompt({
@@ -44,16 +68,112 @@ function addNewDepartment() {
       message: "What is the department name?",
     })
     .then(function (answer) {
-      const query = `INSERT INTO department(name) VALUES ?`;
-      connection.query(query, { department: answer.department }, function (
-        err,
-        res
-      ) {
-        //if (err) throw err;
-        // for (var i = 0; i < res.length; i++) {
-        console.log(res.department);
-        //}
-        //startSearch();
+      const query = `INSERT INTO department(name) VALUES (?)`;
+      connection.query(query, answer.department, function (err, res) {
+        if (err) {
+          throw err;
+        }
+        console.log("Department was added successfully");
+        startSearch();
       });
     });
 }
+
+//view all departments
+function viewDepartments() {
+  const query = "SELECT * FROM department";
+  connection.query(query, function (err, res) {
+    if (err) {
+      throw err;
+    }
+    console.table(res);
+    startSearch();
+  });
+}
+
+function addNewRole() {
+  inquirer
+    .prompt(
+      {
+        name: "role",
+        type: "input",
+        message: "What role would you like to add?",
+      },
+      {
+        name: "salary",
+        type: "input",
+        message: "What is the salary for this role?",
+      },
+      {
+        name: "id",
+        type: "input",
+        message: "What is the department ID?",
+      }
+    )
+    .then(function (answer) {
+      const query = `INSERT INTO role(title,salary, department_id) VALUES (?,?,?)`;
+      connection.query(
+        query,
+        [answer.role, answer.salary, answer.id],
+        function (err, res) {
+          if (err) {
+            throw err;
+          }
+          console.log("A new role was added successfully");
+          //addSalary();
+        }
+      );
+    });
+}
+//add salary
+function addSalary() {
+  inquirer
+    .prompt({
+      name: "salary",
+      type: "input",
+      message: "What is the salary for this role?",
+    })
+    .then(function (answer) {
+      const query = `INSERT INTO role(salary) VALUES (?) WHERE title = ${this.answer.role}`;
+      connection.query(query, answer.salary, function (err, res) {
+        if (err) {
+          throw err;
+        }
+        console.log("Salary was added successfully");
+        startSearch();
+      });
+    });
+}
+
+//view all roles
+function viewAllRoles() {
+  const query = "SELECT * FROM role";
+  connection.query(query, function (err, res) {
+    if (err) {
+      throw err;
+    }
+    console.table(res);
+    startSearch();
+  });
+}
+
+function addEmployee() {
+  inquirer
+    .prompt({
+      name: "firstname",
+      type: "input",
+      message: "What is the first name of the employee?",
+    })
+    .then(function (answer) {
+      const query = `INSERT INTO employee(first_name) VALUES (?) `;
+      connection.query(query, answer.firstname, function (err, res) {
+        if (err) {
+          throw err;
+        }
+        console.log("Employee was added successfully");
+        startSearch();
+      });
+    });
+}
+
+function viewAllEmployees() {}
