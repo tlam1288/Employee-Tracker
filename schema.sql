c57d1050ee8981c95f2b758a2bdf903c30694e5f
nhuanthi <tlam1288@gmail.com>
2020-07-15 16:57:29 -0700

    added more columns to display with join

--- schema.sql
+++ schema.sql
@@ -33,7 +33,8 @@ SELECT
         employee.id AS ID,
         CONCAT(employee.first_name, " ", employee.last_name) AS Name,
         role.title AS Role,
-        department.name AS Department
+        department.name AS Department,
+        employee.manager_id AS ManagerID
       FROM employee
       INNER JOIN role ON employee.role_id = role.id
       INNER JOIN department ON role.department_id = department.id;
