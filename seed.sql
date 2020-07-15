a9fadb8f2ea0ee72d10b2e9a130b9216fe49ae2b
nhuanthi <tlam1288@gmail.com>
2020-07-15 16:54:34 -0700

    updated tables to test employees

--- seed.sql
+++ seed.sql
@@ -2,25 +2,19 @@
 INSERT INTO department(name)
 VALUES 
 ("accounting"),
-("HR"),
-("marketing"),
-("research"),
-("finance"),
-("administration"),
-("engineering"),
-("legal")
+("sales"),
+("engineering")
 ;
 
 -- test for role table
 INSERT INTO role(title, salary, department_id)
-VALUES ("accounting", 80000.00, 1),
-("HR", 75000.00, 2),
-("marketing", 84000.00, 3),
-("research", 79000.00, 4),
-("finance", 100000.00, 5),
-("administration", 65000, 6),
-("engineering", 120000, 7),
-("legal", 105000, 8)
+VALUES ("accountant", 80000.00, 1),
+("sales", 75000.00, 2),
+("marketer", 84000.00, 2),
+("researcher", 79000.00, 3),
+("finance", 100000.00, 1),
+("administration", 65000, 2),
+("engineer", 120000, 3)
 ;
 
 -- test for employee table
@@ -28,4 +22,7 @@ INSERT INTO employee(first_name, last_name, role_id, manager_id)
 VALUES ("John", "Quintos", 1, 11),
 ("Timothy", "Lam", 2, 12),
 ("Joanna", "Bambico", 3, 13),
-("Richie", "Gavino", 4, 14);
+("Richie", "Gavino", 1, 14),
+("Chad", "Codina", 1, 15),
+("Mal", "Tousithi", 3, 16)
+;
