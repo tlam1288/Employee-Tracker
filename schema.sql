02dc6c66d1707b792f0b9abd1ea9fdcf9fe5ff4c
nhuanthi <tlam1288@gmail.com>
2020-07-15 16:54:06 -0700

    added query to join all 3 tables

--- schema.sql
+++ schema.sql
@@ -13,7 +13,8 @@ CREATE TABLE role(
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10,2) NOT NULL,
     department_id INT NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee(
@@ -22,5 +23,17 @@ CREATE TABLE employee(
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
     manager_id INT NOT NULL,
-    PRIMARY KEY (id)  
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role(id)  
 );
+
+-- Joining 3 tables
+
+SELECT
+        employee.id AS ID,
+        CONCAT(employee.first_name, " ", employee.last_name) AS Name,
+        role.title AS Role,
+        department.name AS Department
+      FROM employee
+      INNER JOIN role ON employee.role_id = role.id
+      INNER JOIN department ON role.department_id = department.id;
